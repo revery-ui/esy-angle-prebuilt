10bed5bed67621c8f3e4b9bcf1db6c16e57878ac
bryphe <bryphe@outrunlabs.com>
2020-08-11 11:11:54 -0700

    Hook up build

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # esy-angle-prebuilt
+
 Prebuilt binaries for Google's ANGLE library
+
+Provides `libEGL.dll` and `libGLESV2.dll` from ANGLE: https://github.com/google/angle
+
+The dlls are extracted from the `electron` project, and mingw64-compatible libraries are created with gendef.exe
+
+## License
+
+- Source code is MIT Licensed
+- `libEGL.dll` and `libGLESV2.dll` are licensed according to the Electron terms at https://github.com/electron/electron
+- `gendef.exe` is licensed under the MingW license terms: http://www.mingw.org/license
--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ OS=$1
 
 if [[ $OS == "windows" ]]
 then
+    # Get electron-prebuilt to extract ANGLE dlls from:
+    mkdir _temp
+    curl -LJ https://github.com/electron/electron/releases/download/v9.2.0/electron-v9.2.0-win32-x64.zip -o _temp/electron-prebuilt.zip
+    ls _temp
+    unzip _temp/electron-prebuilt.zip
+    ls _temp
     gendef.exe - $cur__target_dir/libEGL.dll > $cur__target_dir/libEGL.def
     gendef.exe - $cur__target_dir/libGLESv2.dll > $cur__target_dir/libGLESv2.def
     x86_64-W64-mingw32-dlltool.exe -D $cur__target_dir/libEGL.dll -d $cur__target_dir/libEGL.def -A -l $cur__target_dir/libEGL.a
